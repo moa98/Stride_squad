160e3f118fdd5de77a4b00f06abcf8b0f6d6ca44
Marah255 <152785397+MarahMostafa25@users.noreply.github.com>
2024-09-23 11:44:04 +0300

    Adding users to firebase

--- lib/signup_page.dart
+++ lib/signup_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:stridesquad1/user.dart';
 import 'HomePage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class SignUpPage extends StatefulWidget {
   final String gender;
@@ -27,45 +29,54 @@ class _SignUpPageState extends State<SignUpPage> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _userNameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  final TextEditingController _confirmPasswordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
   final TextEditingController _nameController = TextEditingController();
 
   bool _obscurePassword = true;
   bool _obscureConfirmPassword = true;
 
-  Future<void> _signUp() async {
-    if (_formKey.currentState?.validate() ?? false) {
-      try {
-        UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
-          email: _emailController.text,
-          password: _passwordController.text,
-        );
-        // Assuming successful signup, create user model
-        UserModel newUser = UserModel(
-          uid: userCredential.user!.uid,
-          name: _nameController.text,
-          email: _emailController.text,
-          password: _passwordController.text,
-          distanceGoal: 0.0, // Default or ask user to set this
-          gender: widget.gender,
-          fitnessLevel: widget.fitnessLevel,
-          dateOfBirth: int.parse(widget.dateOfBirth), // Make sure to handle date correctly
-          imageUrl: '', // Default or let user upload an image
-          height: widget.height.toString(),
-          weight: widget.weight.toString(),
-        );
-        // Save newUser to Firestore or another database
-        // Navigate to HomePage or success page
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => HomePage()),
-        );
-      } catch (e) {
-        print("Failed to sign up: $e");
-        // Handle errors or show an alert dialog
-      }
+ Future<void> _signUp() async {
+  if (_formKey.currentState?.validate() ?? false) {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: _emailController.text,
+        password: _passwordController.text,
+      );
+      // Extract year from the date string
+      int yearOfBirth = DateTime.parse(widget.dateOfBirth).year;
+
+      // Assuming successful signup, create user model
+      UserModel newUser = UserModel(
+        uid: userCredential.user!.uid,
+        name: _nameController.text,
+        email: _emailController.text,
+        password: _passwordController.text, // Consider not storing passwords in Firestore
+        distanceGoal: 0.0, // Default or ask user to set this
+        gender: widget.gender,
+        fitnessLevel: widget.fitnessLevel,
+        dateOfBirth: yearOfBirth, // Store only the year
+        imageUrl: '', // Default or let user upload an image
+        height: widget.height.toString(),
+        weight: widget.weight.toString(),
+      );
+      // Save newUser to Firestore
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(newUser.uid)
+          .set(newUser.toMap());
+
+      // Navigate to HomePage or success page
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomePage()),
+      );
+    } catch (e) {
+      print("Failed to sign up: $e");
+      // Handle errors or show an alert dialog
     }
   }
+}
 
   @override
   Widget build(BuildContext context) {
@@ -79,22 +90,34 @@ class _SignUpPageState extends State<SignUpPage> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                Text('Sign Up to StrideSquad', textAlign: TextAlign.center, style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+                Text('Sign Up to StrideSquad',
+                    textAlign: TextAlign.center,
+                    style:
+                        TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
                 SizedBox(height: 20),
                 TextFormField(
                   controller: _nameController,
-                  decoration: InputDecoration(labelText: 'Full Name', border: OutlineInputBorder()),
-                  validator: (value) => value != null && value.isNotEmpty ? null : 'Name is required',
+                  decoration: InputDecoration(
+                      labelText: 'Full Name', border: OutlineInputBorder()),
+                  validator: (value) => value != null && value.isNotEmpty
+                      ? null
+                      : 'Name is required',
                 ),
                 TextFormField(
                   controller: _emailController,
-                  decoration: InputDecoration(labelText: 'Email Address', border: OutlineInputBorder()),
-                  validator: (value) => value != null && value.isNotEmpty ? null : 'Email is required',
+                  decoration: InputDecoration(
+                      labelText: 'Email Address', border: OutlineInputBorder()),
+                  validator: (value) => value != null && value.isNotEmpty
+                      ? null
+                      : 'Email is required',
                 ),
                 TextFormField(
                   controller: _userNameController,
-                  decoration: InputDecoration(labelText: 'Username', border: OutlineInputBorder()),
-                  validator: (value) => value != null && value.isNotEmpty ? null : 'Username is required',
+                  decoration: InputDecoration(
+                      labelText: 'Username', border: OutlineInputBorder()),
+                  validator: (value) => value != null && value.isNotEmpty
+                      ? null
+                      : 'Username is required',
                 ),
                 TextFormField(
                   controller: _passwordController,
@@ -103,11 +126,16 @@ class _SignUpPageState extends State<SignUpPage> {
                     labelText: 'Password',
                     border: OutlineInputBorder(),
                     suffixIcon: IconButton(
-                      icon: Icon(_obscurePassword ? Icons.visibility_off : Icons.visibility),
-                      onPressed: () => setState(() => _obscurePassword = !_obscurePassword),
+                      icon: Icon(_obscurePassword
+                          ? Icons.visibility_off
+                          : Icons.visibility),
+                      onPressed: () =>
+                          setState(() => _obscurePassword = !_obscurePassword),
                     ),
                   ),
-                  validator: (value) => value != null && value.isNotEmpty ? null : 'Password is required',
+                  validator: (value) => value != null && value.isNotEmpty
+                      ? null
+                      : 'Password is required',
                 ),
                 TextFormField(
                   controller: _confirmPasswordController,
@@ -116,15 +144,23 @@ class _SignUpPageState extends State<SignUpPage> {
                     labelText: 'Confirm Password',
                     border: OutlineInputBorder(),
                     suffixIcon: IconButton(
-                      icon: Icon(_obscureConfirmPassword ? Icons.visibility_off : Icons.visibility),
-                      onPressed: () => setState(() => _obscureConfirmPassword = !_obscureConfirmPassword),
+                      icon: Icon(_obscureConfirmPassword
+                          ? Icons.visibility_off
+                          : Icons.visibility),
+                      onPressed: () => setState(() =>
+                          _obscureConfirmPassword = !_obscureConfirmPassword),
                     ),
                   ),
-                  validator: (value) => value == _passwordController.text ? null : 'Passwords do not match',
+                  validator: (value) => value == _passwordController.text
+                      ? null
+                      : 'Passwords do not match',
                 ),
                 ElevatedButton(
                   onPressed: _signUp,
-                  style: ElevatedButton.styleFrom(padding: EdgeInsets.symmetric(vertical: 12), shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8))),
+                  style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.symmetric(vertical: 12),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8))),
                   child: Text('Sign Up', style: TextStyle(fontSize: 16)),
                 ),
               ],
--- lib/user.dart
+++ lib/user.dart
@@ -5,25 +5,24 @@ class UserModel {
   String password;
   double distanceGoal;
   String gender;
-  String fitnessLevel; 
+  String fitnessLevel;
   int dateOfBirth;
   String imageUrl;
   String height;
   String weight;
 
-  UserModel({
-    required this.uid,
-    required this.name,
-    required this.email,
-    required this.password,
-    required this.distanceGoal,
-    required this.gender,
-    required this.fitnessLevel,
-    required this.dateOfBirth,
-    required this.imageUrl,
-    required this.height,
-    required this.weight
-  });
+  UserModel(
+      {required this.uid,
+      required this.name,
+      required this.email,
+      required this.password,
+      required this.distanceGoal,
+      required this.gender,
+      required this.fitnessLevel,
+      required this.dateOfBirth,
+      required this.imageUrl,
+      required this.height,
+      required this.weight});
 
   // Convert a UserModel to a map
   Map<String, dynamic> toMap() {
@@ -31,31 +30,34 @@ class UserModel {
       'uid': uid,
       'name': name,
       'email': email,
-      'password': password, 
+      'password': password,
       'distanceGoal': distanceGoal,
       'gender': gender,
       'fitness_level': fitnessLevel,
       'dateOfBirth': dateOfBirth,
       'imageUrl': imageUrl,
-      'height' : height,
-      'weight' : weight,
+      'height': height,
+      'weight': weight,
     };
   }
 
   // Create a UserModel from a map
   factory UserModel.fromMap(Map<String, dynamic> map) {
     return UserModel(
-      uid: map['uid'],
-      name: map['name'],
-      email: map['email'],
-      password: map['password'], // added missing fields
-      distanceGoal: map['distanceGoal'],
-      gender: map['gender'],
-      fitnessLevel: map['fitness_level'],
-      dateOfBirth: map['dateOfBirth'],
-      imageUrl: map['imageUrl'],
-      height: map['height'],
-      weight: map['weight']
-    );
+        uid: map['uid'],
+        name: map['name'],
+        email: map['email'],
+        password: map['password'], // added missing fields
+        distanceGoal: map['distanceGoal'],
+        gender: map['gender'],
+        fitnessLevel: map['fitness_level'],
+        dateOfBirth: map['dateOfBirth'],
+        imageUrl: map['imageUrl'],
+        height: map['height'],
+        weight: map['weight']);
+  }
+
+  Map<String, dynamic> toJson() {
+    return toMap(); // Directly reuse the toMap() function
   }
 }
